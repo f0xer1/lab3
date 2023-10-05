bed946b94bca00241dc14752afdcc246b5822ca2
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-10-05 13:21:51 +0300

    add table page

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const  faqs = document.querySelectorAll(".faq");
+const navBar = document.querySelector("nav"),
+    menuBtns = document.querySelectorAll(".menu-icon"),
+    overlay = document.querySelector(".overlay");
+
 faqs.forEach(faq =>{
     faq.addEventListener("click", ()=>{
         faq.classList.toggle("active")
     })
 })
+
+
+menuBtns.forEach((menuBtn) => {
+    menuBtn.addEventListener("click", () => {
+        navBar.classList.toggle("open");
+    });
+});
+
+overlay.addEventListener("click", () => {
+    navBar.classList.remove("open");
+});
