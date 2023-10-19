98ce0dda666759a7e5f0e858eb37455162c9d35b
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-10-19 22:45:51 +0300

    Add sex radio btn

--- js/sign-up.js
+++ js/sign-up.js
@@ -14,31 +14,41 @@ closeModalButton.addEventListener('click', () => {
     }, 500); // Затримка для закриття анімації
 });
 
-    const form = document.querySelector("form");
-    const tableBody = document.querySelector("#data-table tbody");
 
-    form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Зупинити стандартну обробку форми
+const form = document.querySelector("form");
+const tableBody = document.querySelector("#data-table tbody");
 
-        // Отримайте значення з полів форми
-        const email = document.getElementById("email").value;
-        const lastName = document.getElementById("last_name").value;
-        const firstName = document.getElementById("first_name").value;
-        const telephone = document.getElementById("telephone").value;
-        const group = document.getElementById("group").value;
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        // Створіть новий рядок для таблиці і додайте дані
-        const newRow = tableBody.insertRow();
-        newRow.innerHTML = `
+    const email = document.getElementById("email").value;
+    const lastName = document.getElementById("last_name").value;
+    const firstName = document.getElementById("first_name").value;
+    const telephone = document.getElementById("telephone").value;
+    const group = document.getElementById("group").value;
+
+    // Отримайте вибрану стать
+    const genderInputs = document.querySelectorAll('input[name="gender"]');
+    let selectedGender = "";
+    for (const genderInput of genderInputs) {
+        if (genderInput.checked) {
+            selectedGender = genderInput.value;
+            break;
+        }
+    }
+
+    const newRow = tableBody.insertRow();
+    newRow.innerHTML = `
       <td>${email}</td>
       <td>${lastName}</td>
       <td>${firstName}</td>
       <td>${telephone}</td>
+      <td>${selectedGender}</td>
       <td>${group}</td>
     `;
 
-        // Скинути дані в формі
-        form.reset();
-    });
+    form.reset();
+});
+
 
 
